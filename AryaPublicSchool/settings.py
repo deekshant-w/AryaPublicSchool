86f6b8e62f53f6ccc52de0b57202eaa3cd7f852b
Deekshant Wadhwa <46841177+deekshant-w@users.noreply.github.com>
2023-05-13 09:09:30 -0400

    Firebase Environment Configs changed

--- AryaPublicSchool/settings.py
+++ AryaPublicSchool/settings.py
@@ -101,7 +101,6 @@ if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable.
     DATABASES["default"] = dj_database_url.config(
         conn_max_age=MAX_CONN_AGE, ssl_require=True)
-
     # Enable test database if found in CI environment.
     if "CI" in os.environ:
         DATABASES["default"]["TEST"] = DATABASES["default"]
